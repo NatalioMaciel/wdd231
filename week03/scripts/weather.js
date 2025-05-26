d042a926c4bb2cfd388eb6b9cbc51029043a3220
NatalioMaciel <mac24026@byui.edu>
2025-05-26 19:03:06 -0300

    weather

--- week03/scripts/weather.js
+++ week03/scripts/weather.js
@@ -2,7 +2,7 @@ const currentTemp = document.querySelector('#current-temp');
 const weatherIcon = document.querySelector('#weather-icon');
 const captionDesc = document.querySelector('figcaption');
 
-const url = 'https://api.openweathermap.org/data/2.5/weather?lat=49.75°&lon=6.64°&appid=3801ed2cb99bb12ff5aa950437cd74df&units=metric'
+const url = 'https://api.openweathermap.org/data/2.5/weather?lat=49.75&lon=6.64&appid=3801ed2cb99bb12ff5aa950437cd74df&units=metric'
 
 async function apiFetch() {
     try {
@@ -10,11 +10,19 @@ async function apiFetch() {
         if (response.ok) {
             const data = await response.json();
             console.log(data);
+
+            currentTemp.textContent = `${data.main.temp.toFixed(1)} °C`;
+            const iconSrc = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+            const desc = data.weather[0].description;
+
+            weatherIcon.setAttribute('src', iconSrc);
+            weatherIcon.setAttribute('alt', desc);
+            captionDesc.textContent = desc.charAt(0).toUpperCase() + desc.slice(1);
         }
         else {
             throw Error(await response.text());
         }
-    } catch {
+    } catch (error) {
         console.log(error);
     }
 }
