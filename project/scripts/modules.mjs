c01f5b08844e7e250c432ff5bf522bb3d40a219d
NatalioMaciel <mac24026@byui.edu>
2025-06-13 23:34:39 -0300

    product

--- project/scripts/modules.mjs
+++ project/scripts/modules.mjs
@@ -1,11 +1,14 @@
 import { navigation } from './navigation.mjs';
 import { displayCopyright, displayLastModified } from './date.mjs';
-import { showProducts } from './bestproducts.mjs';
+import { showProducts, loadProducts, loadCategory, filterProducts } from './products.mjs';
 
 
 document.addEventListener("DOMContentLoaded", () => {
     navigation();
     showProducts();
+    loadProducts();
+    loadCategory();
+    filterProducts();
     displayCopyright();
     displayLastModified();
 });
---
+++ project/scripts/products.mjs
@@ -0,0 +1,125 @@
+const container = document.querySelector("#best_products");
+const url = "data/products.json";
+
+function randomArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+}
+
+export async function showProducts() {
+    if (!container) return;
+    try {
+        const response = await fetch(url);
+        if (!response.ok) throw new Error('Data not found');
+
+        const data = await response.json();
+        const products = data.products;
+
+        const filterProducts = products.filter(b => b.sale === "1");
+
+        const bestProducts = randomArray(filterProducts).slice(0, 3);
+
+        bestProducts.forEach(products => {
+            const div = document.createElement("div");
+            div.classList.add("products");
+
+            div.innerHTML = `
+                <h3>${products.productName}</h3>
+                <img src="${products.image}" alt="${products.productName}">
+                <p>${products.category}</p>
+            `;
+            container.appendChild(div);
+        });
+    } catch (error) {
+        console.error('Error:', error);
+    }
+}
+
+
+export function loadCategory() {
+    const display = document.querySelector("#category_products");
+    if (!display) return;
+    fetch('data/categories.json')
+        .then(response => response.json())
+        .then(data => {
+            data.categories.forEach(category => {
+                const div = document.createElement("div");
+                div.classList.add("category");
+
+                div.innerHTML = `
+                    <h3>${category.name}</h3>
+                    <img src="${category.image}" alt="${category.name}">
+                `;
+
+                display.appendChild(div);
+            });
+        })
+        .catch(error => {
+            console.error("Error loading the JSON:", error);
+        });
+}
+
+let allProducts = [];
+
+export async function loadProducts(filteredList = null) {
+    const showProducts = document.querySelector("#products");
+    const url = "data/products.json";
+
+    if (!showProducts) return;
+
+    showProducts.innerHTML = "";
+
+    try {
+        if (allProducts.length === 0) {
+            const response = await fetch(url);
+            const data = await response.json();
+            allProducts = data.products;
+        }
+
+        const productsToDisplay = filteredList || allProducts;
+
+        productsToDisplay.forEach(product => {
+            const card = document.createElement("div");
+
+            const image = document.createElement("img");
+            image.src = product.image;
+            image.alt = product.productName;
+            image.loading = "lazy";
+            card.appendChild(image);
+
+            const title = document.createElement("h2");
+            title.textContent = product.productName;
+            card.appendChild(title);
+
+            const category = document.createElement("h3");
+            category.textContent = product.category;
+            card.appendChild(category);
+
+            const button = document.createElement("button");
+            button.textContent = "Learn More";
+            card.appendChild(button);
+
+            showProducts.appendChild(card);
+        });
+
+    } catch (error) {
+        console.error("Error loading places:", error.message);
+    }
+}
+
+export function filterProducts() {
+
+    const searchInput = document.querySelector('#search');
+
+    searchInput.addEventListener('input', function () {
+        const filter = searchInput.value.toLowerCase();
+        const filteredProducts = allProducts.filter(product =>
+            product.productName.toLowerCase().includes(filter)
+        );
+        loadProducts(filteredProducts);
+    });
+}
+
